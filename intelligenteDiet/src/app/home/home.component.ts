5cef0f7a048c4db069f26ad2bfe8f950e46498c1
manuel <manuel.silva@embedded.ufcg.edu.br>
2018-06-19 20:49:57 -0300

    Criação da view home

--- intelligenteDiet/src/app/home/home.component.ts
+++ intelligenteDiet/src/app/home/home.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  showProfile: boolean = true;
+  showPreferences: boolean = false;
+  showDiet: boolean = false;
+
+  constructor() {
+
+  }
+
+  public setViewToShow = function(view){
+    if(view == 'profile'){
+      console.log('profile');
+      this.showProfile = true;
+      this.showPreferences = false;
+      this.showDiet = false;
+    }else if(view == 'preferences'){
+      console.log('preferences');
+      this.showProfile = false;
+      this.showPreferences = true;
+      this.showDiet = false;
+    }else if(view == 'diet'){
+      console.log('diet');
+      this.showProfile = false;
+      this.showPreferences = false;
+      this.showDiet = true;
+    }
+  }
 
   ngOnInit() {
+
   }
 
 }
